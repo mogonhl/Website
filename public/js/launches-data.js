0cb30de7ee65e62656ed576102ae058dff19ad3e
mogonhl <mog35706@gmail.com>
2025-01-09 14:29:52 +0100

    Launches Initial

--- public/js/launches-data.js
+++ public/js/launches-data.js
@@ -6,9 +6,58 @@ const launchesData = {
             "ticker": "PURRG",
             "name": "THE CAT STAYS ARROGANT.",
             "icon": "/assets/logo.png",
+            "hypurr": "https://app.hypurr.fun/launch/7561",
             "links": {
                 "website": "https://www.purrg.com",
                 "twitter": "https://x.com/purrg_hl"
+            }
+        },
+        {
+            "id": "HAI",
+            "streamId": "AAAAAAcIpk0SAjVt",
+            "ticker": "HAI",
+            "name": "HyperAI Platform",
+            "icon": "https://media.hypurr.fun/AgACAgIAAxkBAAGjC8ZncvFL5hpYpL2LJYN9qFRSKKW45wACFukxG2wzmUtoWeRny7FsGwEAAwIAA3gAAzYE",
+            "hypurr": "https://app.hypurr.fun/launch/9894",
+            "links": {
+                "website": "https://hyperAI.trade",
+                "twitter": "https://x.com/HyperAI_trade",
+                "telegram": "https://t.me/HyperAI_trade"
+            }
+        },
+        {
+            "id": "VORTX",
+            "streamId": "AAAAAAcIsE0SAjVt",
+            "ticker": "VORTX",
+            "name": "Interface for HL combining web and Di...",
+            "icon": "https://media.hypurr.fun/AgACAgEAAxkBAAGjhANncwkVYsenC6fB-2wNLs-SYYhNlgACp60xG75hmEcmP1xf1YSTNAEAAwIAA3gAAzYE",
+            "hypurr": "https://app.hypurr.fun/launch/9904",
+            "links": {
+                "twitter": "https://x.com/vortx_hl"
+            }
+        },
+        {
+            "id": "GUARD",
+            "streamId": "AAAAAAcI11oSAjVt",
+            "ticker": "GUARD",
+            "name": "Chain Guardians",
+            "icon": "https://media.hypurr.fun/AgACAgQAAxkBAAHRAhZnew_DyHoy6QABz_z3ZtqabZYue-4AAlHEMRuI_9lT3W3ODcMmjKsBAAMCAAN5AAM2BA",
+            "hypurr": "https://app.hypurr.fun/launch/11607",
+            "links": {
+                "twitter": "https://x.com/_ChainGuardians"
+            }
+        },
+        {
+            "id": "HYDRA",
+            "streamId": "AAAAAAcIg0kSAjVt",
+            "ticker": "HYDRA",
+            "name": "AI trading agent on Hyperliquid with ML.",
+            "icon": "https://media.hypurr.fun/AgACAgEAAxkBAAG8aF9nd6IjnXxGXAABLqxraoVmdl1XRPEAAlitMRuNccBHP2DHv8Xwf7cBAAMCAAN5AAM2BA",
+            "hypurr": "https://app.hypurr.fun/launch/9347",
+            "links": {
+                "website": "https://www.agenthydra.com",
+                "twitter": "https://x.com/AgentHydra_com",
+                "telegram": "https://t.me/Agent_HYDRA"
             }
         }
     ]
